0a91f00969ac7a81315f6158bdbc376d5700d8b6
mattbeedle <mattbeedle@gmail.com>
2016-11-06 14:01:08 +0700

    yet more tweaking

--- vim/install.sh
+++ vim/install.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
-cd ~
-git clone git://github.com/zaiste/vimified.git
-ln -sfn vimified/ ~/.vim
-ln -sfn vimified/vimrc ~/.vimrc
-cd vimified
+if [ -d "$HOME/vimified"]; then
+    cd ~/vimified
+    git fetch
+else
+    cd ~
+    git clone git://github.com/zaiste/vimified.git ~/vimified
+    ln -sfn vimified/ ~/.vim
+    ln -sfn vimified/vimrc ~/.vimrc
+    cd vimified
 
-mkdir bundle
-mkdir -p tmp/backup tmp/swap tmp/undo
+    mkdir bundle
+    mkdir -p tmp/backup tmp/swap tmp/undo
 
-git clone https://github.com/gmarik/vundle.git bundle/vundle
+    git clone https://github.com/gmarik/vundle.git ~/vimified/bundle/vundle -f
+fi
+
 echo "let g:vimified_packages = ['general', 'fancy', 'css', 'js', 'os', 'html', 'coding', 'color', 'ruby']" > local.vimrc
 
-ln -s ~/.extra.vimrc ~/vimified/extra.vimrc
-ln -s ~/.after.vimrc ~/vimified/after.vimrc
+ln -sfn ~/.extra.vimrc ~/vimified/extra.vimrc
+ln -sfn ~/.after.vimrc ~/vimified/after.vimrc
 
 vim +BundleInstall +qall 2>/dev/null
